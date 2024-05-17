2fd50d244a93705fb64c57268770c77e56886c26
Riley-Grindle <122935497+Riley-Grindle@users.noreply.github.com>
2024-05-17 13:39:06 -0400

    MakeSeurat.R

---
+++ 04_WorkingScripts/MakeSeurat.R
@@ -0,0 +1,112 @@
+#!/usr/local/bin/Rscript
+
+
+# title: "SeuratV5_Normalize_QC.Rmd"
+# author: "Ryan Seaman"
+# date: "02/06/2024"
+
+#############################################################################################################################################################################
+
+##################
+# LOAD LIBRARIES #
+##################
+
+library(dplyr)
+library(Matrix)
+library(viridis)
+library(tidyverse)
+library(Seurat)
+library(SeuratData)
+library(SeuratObject)
+library(SeuratWrappers)
+library(Seurat.utils)
+library(SingleCellExperiment)
+library(gprofiler2)
+library(stringr)
+library(patchwork)
+library(presto)
+
+##################################
+# READ IN PARAMS AND DIRECTORIES #
+##################################
+
+args <- commandArgs(trailingOnly = TRUE)
+
+# File path to features, barcodes, mtx
+params.data_directory <- args[1]
+
+# Which column to use for Seurat Obj. instantiation
+params.gene_identifier <- args[2]
+
+# File path for target gene list
+params.genes_2_rm <- args[3]
+
+# Sample Name
+params.sample_name <- args[4]
+
+# Project Name (analysis group)
+params.project_name <- args[5]
+
+# Min Cells
+params.min_cells <- args[6]
+
+# Min Features
+params.min_features <- args[7]
+
+
+######################
+# FEATURE SUBSETTING #
+######################
+
+if (params.genes_2_use == null){
+  feature_list  <- read.csv(paste0(params.data_directory, "features.tsv"), sep = "\t", header = F)
+  new_feat_list <- feature_list
+}else{
+  gene_list     <- read.csv(params.genes_2_rm, sep = "\t", header = F)
+  feature_list  <- read.csv(paste0(params.data_directory, "features.tsv"), sep = "\t", header = F)
+  new_feat_list <- feature_list[-c(which(feature_list %in% gene_list))]
+}
+
+
+################################
+# CREATE 10X OBJECT AND SUBSET #
+################################
+
+if (toupper(params.gene_identifier) == "GENE_ID"){
+  params.gene_column <- 1
+}else if (toupper(params.gene_identifier) == "GENE_NAME"){
+  params.gene_column <- 2
+}else {
+  params.gene_column <- 2
+}
+
+# Create raw 10x object
+Name10X <- params.sample_name
+assign(Name10X, Read10X(data.dir = params.data_directory, strip.suffix = T, gene.column = params.gene_column))
+
+# Subset 10x Object
+Name10XAnnotated <- paste0(params.sample_name,"_Ann")
+assign(Name10XAnnotated, get(params.sample_name)[which(rownames(get(params.sample_name)) %in% rownames(new_feat_list)),])
+
+########################
+# CREATE SEURAT OBJECT #
+########################
+
+NameSO <- paste0("SO_",params.sample_name)
+assign(NameSO, CreateSeuratObject(counts = get(Name10XAnnotated), project = params.project_name, min.cells = params.min_cells, min.features = params.min_features))
+
+###############
+# SAVE OUTPUT #
+###############
+
+SaveSeuratRds(get(NameSO), file = paste0(params.sample_name, "_SO.rds"))
+
+sink(paste0(params.sample_name,"validation.log"))
+
+print(get(NameSO))
+
+sink()
+
+#####################################################################################################################################################################################
+
+

382102cd77e18e11be8540db61cb75f28d54d944
Ryan <rseaman@mdibl.org>
2024-05-24 12:46:31 -0400

    initial addition of DoubletFinder and Merged (not tested)

--- 00_InitialScripts/single_cell/DoubletFinder.R
+++ 00_InitialScripts/single_cell/DoubletFinder.R
@@ -1,135 +1,143 @@
-# Takes the single sample seurat objects through a 
-# process of finding doublets (Using Doublet Finder)
-# and removing the predicted doublets based on the 
-# expected number of cells for the sample. 
+#!/usr/local/bin/Rscript
 
-# command: Rscript DoubletFinder.R <SeuratObject>.rds <metaid> <params.vars2Regress> <data_dir>
+# title: "Doublet_Finder.R"
 
-## read in the parameters 
+# function: 
+#   Takes the single sample Seurat objects through a 
+#   process of finding doublets (Using Doublet Finder)
+#   and removing the predicted doublets based on the 
+#   expected number of cells for the sample. 
+
+
+#############################################################################################################################################################################
+
+##################
+# LOAD LIBRARIES #
+##################
+
+library(dplyr)
+library(Matrix)
+library(viridis)
+library(tidyverse)
+library(Seurat)
+library(SeuratData)
+library(SeuratObject)
+library(SeuratWrappers)
+library(Seurat.utils)
+library(SingleCellExperiment)
+library(gprofiler2)
+library(stringr)
+library(patchwork)
+library(presto)
+
+##################################
+# READ IN PARAMS AND DIRECTORIES #
+##################################
+
 args <- commandArgs(trailingOnly = TRUE)
 
+# RDS file from QC
+params.SeuratObject <- args[1]
+
+# Variables To Regress in the Scaling
+params.VarsToRegress <- args[2]
+
+# Number of PCs to use, default to auto
+params.PCs <- args[3]
+
+# Data Dir
+params.DataDir <- args[4]
+
+# SampleName
+params.sampleName <- args[5]
+
+#############################
+# Find Doublets Preperation #
+#############################
+
 # read in .rds  
-samp <- readRDS(args[1])
+samp <- readRDS(params.SeuratObject)
 
 # create a list of all genes
 all.genes <- rownames(samp)
 
 # Scale Data
-samp <- ScaleData(samp, features = all.genes ,vars.to.regress = args[3])
+samp <- ScaleData(samp, features = all.genes ,vars.to.regress = params.VarsToRegress)
 
 
-#### Run PCA on individual samples
+# Run PCA on individual samples
 samp <- RunPCA(samp, features = all.genes ,npcs = 100)
 
-#### Generate Elbow Plots and Calculate PCs to use
+# Generate Elbow Plots and Calculate PCs to use
 Elbow <- ElbowPlot(samp, ndims = 100, reduction = "pca")
-pdf(paste0(args[2],"_ElbowPlot.pdf"), width = 20, height = 15)
-pcCount <- 1
-while(Elbow$data$stdev[pcCount]-Elbow$data$stdev[pcCount+1]>0.01 | Elbow$data$stdev[pcCount+1]-Elbow$data$stdev[pcCount+2]>0.01){
-    pcCount <- pcCount + 1
+pdf(paste0(params.sampleName,"_ElbowPlot.pdf"), width = 20, height = 15)
+
+if (params.PCs == "auto"){
+    pcCount <- 1
+    while(Elbow$data$stdev[pcCount]-Elbow$data$stdev[pcCount+1]>0.01 | Elbow$data$stdev[pcCount+1]-Elbow$data$stdev[pcCount+2]>0.01){
+        pcCount <- pcCount + 1
+    }
+    params.pcMax <- pcCount
+}else{
+    params.pcMax <- params.PCs
 }
-param.pcMax <- pcCount
 
-##### STOPPED HERE
-#### Find Neighbors
+# Find Neighbors 
 
-```{r}
-count <- 1
-for (samp in SeuratObjList){
-    print(paste0("Running Find Neighbors on Sample: ", samp))
-    assign(samp,FindNeighbors(get(samp), dims = 1:param.pcMax[count], reduction = "pca"))
-    count <- count + 1
-}
-```
+samp <- FindNeighbors(samp, dims = 1:params.pcMax, reduction = "pca")
 
-#### Find Clusters
+# Find Clusters
+samp <- FindClusters(samp)
 
-```{r}
-for (samp in SeuratObjList){
-    print(paste0("Running Find Clusters on Sample: ", samp))
-    assign(samp,FindClusters(get(samp)))
-}
-```
+# Build UMAP
+samp <- RunUMAP(samp, dims = 1:params.pcMax, reduction = "pca")
 
-#### Build UMAP
+# Get the number of recovered cells
+params.CellsRecovered <- length(readLines((paste0(params.DataDir,list.files(path = params.DataDir, pattern = "barcodes")))))
 
-```{r}
-count <- 1
-for (samp in SeuratObjList){
-    print(paste0("Building UMAP on Sample: ", samp))
-    assign(samp,RunUMAP(get(samp), dims = 1:param.pcMax[count], reduction = "pca"))
-    count <- count + 1
-}
-```
+#####################
+# Identify Doublets #
+#####################
+#    (Percent Doublet is calculated based on a 0.8% increase per 1000 cells recovered as per the 10X website.)
+sweep.res.list_meta <- paramSweep(samp, PCs = 1:params.pcMax, sct = F )
+sweep.stats_meta <- summarizeSweep(sweep.res.list_meta, GT = FALSE)
+bcmvn_meta <- find.pK(sweep.stats_meta)
+pK <- bcmvn_meta %>%
+    filter(BCmetric == max(BCmetric)) %>%
+    select(pK)
+pK <- as.numeric(as.character(pK[[1]]))
+annotations <- samp$seurat_clusters
+homotypic.prop <- modelHomotypic(annotations)
+nExp_poi <- round(((params.CellsRecovered/1000)*0.008)*nrow(samp@meta.data))
+nExp_poi.adj <- round(nExp_poi*(1 - homotypic.prop))
+samp <- doubletFinder(samp,
+                      PCs = 1:params.pcMax,
+                      pN = 0.25,
+                      pK = pK,
+                      nExp = nExp_poi.adj,
+                      reuse.pANN = F,
+                      sct = F))
 
-### Identify Doublets
 
-Percent Doublet is calculated based on a 0.8% increase per 1000 cells recovered as per the 10X website.
+# Visualize and Count Doublets
+doubletmeta <- as.character(colnames(samp[[grep("DF",(colnames(get(samp)[[]])))]]))
+DimPlot(samp, group.by = doubletmeta)+ ggtitle(samp)
+pdf(paste0(params.sampleName,"VisualizeDoublets.pdf"), width = 20, height = 15)
+DoubletCount <- table(samp[[doubletmeta]])
 
-```{r}
-count <- 1 
-for (samp in SeuratObjList){
-    print(paste0("Finding Doublets on Sample: ", samp))
-    print(paste0("    Proportion of Doublet Cells: ",(params.CellsRecovered[count]/1000)*0.008))
+########################
+# Subset Seurat Object #
+########################
+samp <- subset(samp, subset = !!as.name(doubletmeta) == "Singlet"  ))
 
-    sweep.res.list_meta <- paramSweep(get(samp), PCs = 1:param.pcMax[count], sct = F )
-    sweep.stats_meta <- summarizeSweep(sweep.res.list_meta, GT = FALSE)
-    bcmvn_meta <- find.pK(sweep.stats_meta)
+######################
+# Save Seuart Object #
+######################
+SaveSeuratRds(get(samp), file = paste0(params.sampleName, "_SO.rds"))
 
-    print(ggplot(bcmvn_meta, aes(pK, BCmetric, group = 1))+ geom_point() + geom_line()+ ggtitle(samp))
+sink(paste0(params.sampleName,"validation.log"))
 
-    pK <- bcmvn_meta %>%
-        filter(BCmetric == max(BCmetric)) %>%
-        select(pK)
+print(DoubletCount)
+samp
 
-    pK <- as.numeric(as.character(pK[[1]]))
-    
-    print(paste0("    pK set at : ",pK))
-
-    annotations <- get(samp)$seurat_clusters
-    homotypic.prop <- modelHomotypic(annotations)
-    nExp_poi <- round(((params.CellsRecovered[count]/1000)*0.008)*nrow(get(samp)@meta.data))
-    nExp_poi.adj <- round(nExp_poi*(1 - homotypic.prop))
-
-    assign(paste0(samp), doubletFinder(get(samp),
-                                        PCs = 1:param.pcMax[count],
-                                        pN = 0.25,
-                                        pK = pK,
-                                        nExp = nExp_poi.adj,
-                                        reuse.pANN = F,
-                                        sct = F))
-    count <- count + 1
-}
-```
-
-#### Visualize Doublets
-
-```{r}
-for (samp in SeuratObjList){
-    print(paste0("Visualizing Doublets on Sample: ", samp))
-    doubletmeta <- as.character(colnames(get(samp)[[grep("DF",(colnames(get(samp)[[]])))]]))
-    print(DimPlot(get(samp), group.by = doubletmeta)+ ggtitle(samp)) 
-}
-```
-
-#### Count Doublets
-
-```{r}
-for (samp in SeuratObjList){
-    print(paste0("Counting Doublets and Singlets on Sample: ", samp))
-    doubletmeta <- as.character(colnames(get(samp)[[grep("DF",(colnames(get(samp)[[]])))]]))
-    print(table(get(samp)[[doubletmeta]]))
-}
-```
-
-#### Subset For Singlets
-
-```{r}
-for (samp in SeuratObjList){
-    print(paste0("Subsetting for Singlets on Sample: ", samp))
-    doubletmeta <- as.character(colnames(get(samp)[[grep("DF",(colnames(get(samp)[[]])))]]))
-    assign(samp,subset(get(samp), subset = !!as.name(doubletmeta) == "Singlet"  ))
-}
-```
-
-### Merge Seurat Objects
+sink()
---
+++ 04_WorkingScripts/Merge.R
@@ -0,0 +1,117 @@
+#!/usr/local/bin/Rscript
+
+# title: "Merge.R"
+
+# function: 
+#   Takes all of the Seurat Objects (post Doublet Finder) 
+#   that belong to a specific `analysis group` and 
+#   runs the following steps: 
+#       - Merge
+#       - Find Variable Features
+#       - Generate QC Plots
+#       - Scale Data
+
+
+#############################################################################################################################################################################
+
+##################
+# LOAD LIBRARIES #
+##################
+
+library(dplyr)
+library(Matrix)
+library(viridis)
+library(tidyverse)
+library(Seurat)
+library(SeuratData)
+library(SeuratObject)
+library(SeuratWrappers)
+library(Seurat.utils)
+library(SingleCellExperiment)
+library(gprofiler2)
+library(stringr)
+library(patchwork)
+library(presto)
+
+##################################
+# READ IN PARAMS AND DIRECTORIES #
+##################################
+
+args <- commandArgs(trailingOnly = TRUE)
+
+# Project Name for this analysis group
+params.ProjectName <- args[1]
+
+# Variable To Regress For Scaling
+params.vars2regress <- args[2]
+
+##############################################
+# Read in and Merge Seurat Objects from .rds #
+##############################################
+SeuratRDSfiles <- list.files(pattern = "\\.rds$", ignore.case = T)
+
+SampleNames <- c()
+
+counter <- 1 
+for( file in SeuratRDSfiles){
+    ObjName <- gsub("_.*","", file)
+    SampleNames <- append(SampleNames,ObjName)
+    assign(ObjName, readRDS(file))
+}
+
+count <- 2
+countMax <- length(SampleNames)
+SOlist2up <- c()
+while (count <= countMax){
+    SOlist2up <- append(SOlist2up, get(SampleNames[count]))
+    count <- count + 1
+}
+
+MergedSO <- merge(get(SampleNames[1]), y= SOlist2up, add.cell.ids = SampleNames, project = params.ProjectName)
+MergedSO
+
+##################################
+# Normalize Merged Seurat Object #
+##################################
+MergedSO <- NormalizeData(MergedSO)
+
+###############################################
+# Find Variable Genes on Merged Seurat Object #
+###############################################
+MergedSO <- FindVariableFeatures(MergedSO)
+
+################################################
+#Save Visualization of Merged Post Filter Plot #
+################################################
+plot1 <- FeatureScatter(MergedSO, feature1 = "nCount_RNA", feature2 = "percent.mt", shuffle = T, group.by = "orig.ident")
+plot2 <- FeatureScatter(MergedSO, feature1 = "nCount_RNA", feature2 = "nFeature_RNA", shuffle = T, group.by = "orig.ident")
+vplot1 <- VlnPlot(MergedSO, features = c("nCount_RNA","nFeature_RNA","percent.mt"),pt.size = -1, group.by = "orig.ident")
+
+pdf(file = paste0(params.ProjectName,'postMergeQC.pdf'), title = paste0(params.ProjectName,' Post Merge QC'), width = 11, height = 8.5)
+plot1 + plot2
+vplot1
+dev.off()
+
+##############################
+# Scale Merged Seurat Object #
+##############################
+all.genes <- rownames(MergedSO)
+MergedSO = ScaleData(MergedSO, features = all.genes ,vars.to.regress = c("nCount_RNA","nFeature_RNA","percent.mt","S.Score", "G2M.Score")) 
+
+#############################################
+# Save Merged + Scaled Seurat Object to RDS #
+#############################################
+if(param.saveRdsSO == T){
+    if(!dir.exists("RDSObjects")){
+        dir.create("RDSObjects")
+    }
+    saveRDS(MergedSO, paste0(params.ProjectName,"SO.rds"))
+}
+
+sink("Mergedvalidation.log")
+
+print(get(MergedSO))
+
+sink()
+
+

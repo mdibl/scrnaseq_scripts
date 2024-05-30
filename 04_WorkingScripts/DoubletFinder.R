91b0ec8fa0686a7cc9c62d2a0ec84356ac04bd73
Ryan <rseaman@mdibl.org>
2024-05-30 11:26:36 -0400

    Update DoubletFinder.R

--- 04_WorkingScripts/DoubletFinder.R
+++ 04_WorkingScripts/DoubletFinder.R
@@ -33,6 +33,7 @@ library(stringr)
 library(patchwork)
 library(loupeR)
 library(presto)
+library(findPC)
 
 ##################################
 # READ IN PARAMS AND DIRECTORIES #
@@ -48,17 +49,11 @@ params.VarsToRegress <- args[2]
 params.VarsToRegress <- unlist(strsplit(params.VarsToRegress, ","))
 print(params.VarsToRegress)
 
-# Number of PCs to use, default to auto
-params.PCs <- args[3]
-if (params.PCs != "auto"){
-    params.PCs <- as.integer(params.PCs)
-}
-
 # Data Dir
-params.DataDir <- args[4]
+params.DataDir <- args[3]
 
 # SampleName
-params.sampleName <- args[5]
+params.sampleName <- args[4]
 
 #############################
 # Find Doublets Preperation #
@@ -78,18 +73,71 @@ samp <- ScaleData(samp, features = all.genes ,vars.to.regress = params.VarsToReg
 samp <- RunPCA(samp, features = all.genes ,npcs = 100)
 
 # Generate Elbow Plots and Calculate PCs to use
-Elbow <- ElbowPlot(samp, ndims = 100, reduction = "pca")
-pdf(paste0(params.sampleName,"_ElbowPlot.pdf"), width = 20, height = 15)
+Elbow <- ElbowPlot(samp,  ndims = 100, reduction = "pca")
+ElbowPoints <- Elbow$data
 
-if (params.PCs == "auto"){
-    pcCount <- 1
-    while(Elbow$data$stdev[pcCount]-Elbow$data$stdev[pcCount+1]>0.01 | Elbow$data$stdev[pcCount+1]-Elbow$data$stdev[pcCount+2]>0.01){
-        pcCount <- pcCount + 1
-    }
-    params.pcMax <- pcCount
-}else{
-    params.pcMax <- params.PCs
+for (i in seq(0.1, 0.9, 0.1)){
+    loess <- loess(stdev ~ dims,data=ElbowPoints, span = i)
+    ElbowPoints[paste0("loessS", i)] <- loess$fitted
 }
+
+for (i in seq(0.1, 0.9, 0.1)){
+    first_deriv <- diff(ElbowPoints[,paste0("loessS", i)])/diff(ElbowPoints$dims)
+    ElbowPoints[3:100,paste0("stddev_Der2_", i)]<- (diff(first_deriv)/diff(ElbowPoints$dims))[1:length(diff(first_deriv)/diff(ElbowPoints$dims))-1]
+}
+
+columns <- colnames(ElbowPoints)
+idents <- seq(0.1, 0.9, 0.1)
+SSE <- c()
+deriv2_var <- c()
+for (item in columns){
+    if (length(grep("Der2", item)) > 0){
+        variance <- var(ElbowPoints[item], na.rm = T)
+        deriv2_var <- append(deriv2_var, variance)
+    }else if (length(grep("loess", item)) > 0 & !(length(grep("Der", item)) > 0)){
+        err <- ElbowPoints[item] - ElbowPoints$stdev
+        norms <- sum(((err - min(err))**2)/((max(err) - min(err))**2))
+        SSE <- append(SSE, norms)
+    }
+}
+
+rm_ind <- which(deriv2_var > 0.010)
+if (length(rm_ind) > 0){
+    SSE <- SSE[-rm_ind]
+    deriv2_var <- deriv2_var[-rm_ind]
+    idents <- idents[-rm_ind]
+    plot(SSE, deriv2_var)
+    index <- which(SSE == min(SSE))
+    ident <- paste0('loessS', idents[index])
+}else {
+    index <- 1
+    ident <- paste0('loessS', idents[index])
+}
+
+x = ElbowPoints$dims
+y = ElbowPoints$stdev
+df <- data.frame(x,y)
+
+pc_tbl <- findPC(sdev = ElbowPoints[[ident]], number = 100, method = "all", figure = T)
+params.pcMax <- mean(x = c(pc_tbl[1,2], pc_tbl[1,3], pc_tbl[1,4]))
+
+pdf(paste0(params.sampleName,"_ElbowPlot.pdf"), width = 20, height = 15)
+ggplot(df, aes(x, y)) +
+    geom_point() +
+    geom_line(aes(x = seq(1,100), y = ElbowPoints[[ident]]), color = "green") +
+    xlab("PC") +
+    ylab("Std Dev") +
+    geom_vline(xintercept = params.pcMax, linetype="dotted", 
+               color = "red", size=1.5) +
+    ggtitle(paste0("Loess Regression of Std Dev ~ PC  :  PC Chosen = ", params.pcMax))
+dev.off()
+
+############ END OF NEW FIND PC
+
+
+
+
+
 
 # Find Neighbors 
 
